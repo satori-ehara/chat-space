ce4b8a2b6bbb9fcac325357dffc6ebec9c9b9b0f
satori-ehara <satori.0701191@gmail.com>
2020-07-04 16:36:40 +0900

    メッセージ送信機能追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :message, presence: true, unless: :image?
+  validates :messages, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_051345) do
+ActiveRecord::Schema.define(version: 2020_07_04_072152) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_07_04_051345) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
     t.index ["group_id"], name: "index_messages_on_group_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
