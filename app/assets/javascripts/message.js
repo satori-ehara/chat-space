9cff576779b2908b66d5614850aabb2f167d681c
satori-ehara <satori.0701191@gmail.com>
2020-07-09 12:30:04 +0900

    message.jsにajaxの雛形追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,13 @@ $(function(){
   $(".form").on("submit", function(e){
     console.log("ok");
     e.preventDefault();
+    $.ajax({
+      url: 取得したリクエストURL,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: 取得したFormData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
