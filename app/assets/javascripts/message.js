f5db109bad40fe914b1516e64b23bb9cd3326716
satori-ehara <satori.0701191@gmail.com>
2020-07-09 15:14:50 +0900

    非同期通信にて、メッセージボックスに新規投稿メッセージを追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,49 @@
 $(function(){
+
+  function buildHTML(message){
+    console.log(message.image.url);
+    if ( message.image.url ) {
+      console.log("true");
+      let html =
+        `<div class="main__message-list--block">
+          <div class="header">
+            <div class=".header__name">
+              ${message.user_name}
+            </div>
+            <div class="header__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="body">
+            <p class="body__content">
+              ${message.message}
+            </p>
+            <img class="Message__image" src="${message.image.url}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      console.log("false");
+      let html =
+      `<div class="main__message-list--block">
+        <div class="header">
+          <div class=".header__name">
+            ${message.user_name}
+          </div>
+          <div class="header__date">
+          ${message.created_at}
+          </div>
+        </div>
+        <div class="body">
+          <p class="body__content">
+            ${message.message}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   $(".form").on("submit", function(e){
     console.log("ok");
     e.preventDefault();
@@ -11,6 +56,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.main__message-list').append(html);
+      $('.main__message-list').animate({ scrollTop: $('.main__message-list')[0].scrollHeight});
+      $('form')[0].reset();
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
-json.messages  @message.messages
-json.image  @message.image
-json.user_id  @message.user.id
-json.group_id  @message.group.id
+json.message @message.messages
+json.image @message.image
+json.user_name @message.user.name
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
