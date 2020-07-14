478ba1916579d85452b6bb1091958d024a45a860
satori-ehara <satori.0701191@gmail.com>
2020-07-14 17:17:04 +0900

    メッセージの最初の投稿の自動更新ができていなかった問題を解決

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,6 +2,9 @@ class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id]
+    if last_message_id == nil then
+      last_message_id = 0
+    end
     @messages = group.messages.includes(:user).where("id > ?", last_message_id)
   end
 end
